d9024620ecd51c4f94f12fec9fb774d4643bfd99
Yaroslav82 <yargay230607@gmail.com>
2024-01-09 14:13:57 +0200

    Replace ResponseEntity with NotFoundException

--- school-manager-server/src/main/java/com/hillel/multi/service/utils/ListToResponseConverter.java
+++ school-manager-server/src/main/java/com/hillel/multi/service/utils/ListToResponseConverter.java
@@ -1,5 +1,6 @@
 package com.hillel.multi.service.utils;
 
+import com.hillel.multi.configuration.exceptions.NotFoundException;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
@@ -9,9 +10,7 @@ public class ListToResponseConverter {
 
     public static <T> ResponseEntity<List<T>> convert(List<T> entityList) {
         if (Objects.isNull(entityList)) {
-            return ResponseEntity.internalServerError().build();
-        } else if (entityList.isEmpty()) {
-            return ResponseEntity.notFound().build();
+            throw new NotFoundException();
         } else {
             return ResponseEntity.ok(entityList);
         }
