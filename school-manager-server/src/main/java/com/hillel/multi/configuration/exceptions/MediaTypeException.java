6169fc20b7278d68bd88b2ad2ae42c88d665a13b
Yaroslav82 <yargay230607@gmail.com>
2024-01-11 12:54:28 +0200

    Add ResponseStatus annotations

--- school-manager-server/src/main/java/com/hillel/multi/configuration/exceptions/MediaTypeException.java
+++ school-manager-server/src/main/java/com/hillel/multi/configuration/exceptions/MediaTypeException.java
@@ -1,5 +1,9 @@
 package com.hillel.multi.configuration.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
 public class MediaTypeException extends RuntimeException {
 
     private final static String DEFAULT_RESPONSE_MESSAGE = "Unsupported Media Type.";
