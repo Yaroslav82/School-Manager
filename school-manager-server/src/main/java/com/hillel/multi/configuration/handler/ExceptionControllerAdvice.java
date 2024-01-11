7f42f9cda65d4a781074d45a43f7eae94fdcf1be
Yaroslav82 <yargay230607@gmail.com>
2024-01-11 12:57:02 +0200

    Refactor three methods in one

--- school-manager-server/src/main/java/com/hillel/multi/configuration/handler/ExceptionControllerAdvice.java
+++ school-manager-server/src/main/java/com/hillel/multi/configuration/handler/ExceptionControllerAdvice.java
@@ -2,31 +2,23 @@ package com.hillel.multi.configuration.handler;
 
 import com.hillel.multi.configuration.exceptions.ErrorDetails;
 import com.hillel.multi.configuration.exceptions.InternalServerException;
-import com.hillel.multi.configuration.exceptions.NotFoundException;
 import com.hillel.multi.configuration.exceptions.MediaTypeException;
+import com.hillel.multi.configuration.exceptions.NotFoundException;
+import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ExceptionControllerAdvice {
 
-    @ExceptionHandler(NotFoundException.class)
-    public final ResponseEntity<ErrorDetails> handleNotFoundException(Exception ex) {
-        ErrorDetails errorDetails = new ErrorDetails(ex.getMessage(),  HttpStatus.NOT_FOUND.value());
-        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler(MediaTypeException.class)
-    public final ResponseEntity<ErrorDetails> handleMediaTypeException(Exception ex) {
-        ErrorDetails errorDetails = new ErrorDetails(ex.getMessage(),  HttpStatus.UNSUPPORTED_MEDIA_TYPE.value());
-        return new ResponseEntity<>(errorDetails, HttpStatus.UNSUPPORTED_MEDIA_TYPE);
-    }
-
-    @ExceptionHandler(InternalServerException.class)
-    public final ResponseEntity<ErrorDetails> handleInternalServerException(Exception ex) {
-        ErrorDetails errorDetails = new ErrorDetails(ex.getMessage(),  HttpStatus.INTERNAL_SERVER_ERROR.value());
-        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    @ExceptionHandler({NotFoundException.class, MediaTypeException.class, InternalServerException.class})
+    public final ResponseEntity<ErrorDetails> handleExceptions(Exception ex) {
+        ResponseStatus responseStatus = AnnotationUtils.findAnnotation(ex.getClass(), ResponseStatus.class);
+        HttpStatus status = responseStatus != null ? responseStatus.code() : HttpStatus.INTERNAL_SERVER_ERROR;
+        ErrorDetails errorDetails = new ErrorDetails(ex.getMessage(), status.value());
+        return new ResponseEntity<>(errorDetails, status);
     }
 }
