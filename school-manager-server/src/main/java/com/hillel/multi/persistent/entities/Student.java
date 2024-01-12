29bf2ecf15a04a904f93af4aca2585a864f9ded2
Yaroslav82 <yargay230607@gmail.com>
2024-01-12 21:49:42 +0200

    Create Student entity

---
+++ school-manager-server/src/main/java/com/hillel/multi/persistent/entities/Student.java
@@ -0,0 +1,28 @@
+package com.hillel.multi.persistent.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "students")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "group")
+    private String group;
+}
