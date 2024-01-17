0366563c8a3da77d6fa8ba62c1b32d4636be96be
Yaroslav82 <yargay230607@gmail.com>
2024-01-17 14:51:51 +0200

    Add validation to fields

--- school-manager-server/src/main/java/com/hillel/multi/persistent/entities/Homework.java
+++ school-manager-server/src/main/java/com/hillel/multi/persistent/entities/Homework.java
@@ -1,6 +1,10 @@
 package com.hillel.multi.persistent.entities;
 
+import com.hillel.multi.persistent.annotations.ValidGroupName;
 import jakarta.persistence.*;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -23,21 +27,28 @@ public class Homework {
     private Integer id;
 
     @Column(name = "name")
+    @NotBlank
     private String name;
 
     @Column(name = "description")
+    @NotBlank
     private String description;
 
     @Column(name = "group_name")
-    private String group;
+    @ValidGroupName
+    private String groupName;
 
     @Column(name = "subject")
+    @NotBlank
     private String subject;
 
     @Column(name = "deadline")
+    @Pattern(regexp = "^([12]\\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\\d|3[01]))$")
+    @NotBlank
     private String deadline;
 
     @ManyToMany(mappedBy = "homeworks")
+    @Valid
     private Set<Student> students;
 
     @Override
@@ -45,11 +56,11 @@ public class Homework {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Homework homework = (Homework) o;
-        return Objects.equals(name, homework.name) && Objects.equals(description, homework.description) && Objects.equals(group, homework.group) && Objects.equals(subject, homework.subject) && Objects.equals(deadline, homework.deadline);
+        return Objects.equals(name, homework.name) && Objects.equals(description, homework.description) && Objects.equals(groupName, homework.groupName) && Objects.equals(subject, homework.subject) && Objects.equals(deadline, homework.deadline);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, description, group, subject, deadline);
+        return Objects.hash(name, description, groupName, subject, deadline);
     }
 }
--- school-manager-server/src/main/java/com/hillel/multi/persistent/entities/Student.java
+++ school-manager-server/src/main/java/com/hillel/multi/persistent/entities/Student.java
@@ -1,6 +1,9 @@
 package com.hillel.multi.persistent.entities;
 
+import com.hillel.multi.persistent.annotations.ValidGroupName;
 import jakarta.persistence.*;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -23,13 +26,16 @@ public class Student {
     private Integer id;
 
     @Column(name = "first_name")
+    @NotBlank
     private String firstName;
 
     @Column(name = "last_name")
+    @NotBlank
     private String lastName;
 
     @Column(name = "group_name")
-    private String group;
+    @ValidGroupName
+    private String groupName;
 
     @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     @JoinTable(
@@ -37,6 +43,7 @@ public class Student {
             joinColumns = @JoinColumn(name = "id1", referencedColumnName = "id"),
             inverseJoinColumns = @JoinColumn(name = "id2", referencedColumnName = "id")
     )
+    @Valid
     private Set<Homework> homeworks;
 
     @Override
@@ -44,11 +51,11 @@ public class Student {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Student student = (Student) o;
-        return Objects.equals(firstName, student.firstName) && Objects.equals(lastName, student.lastName) && Objects.equals(group, student.group);
+        return Objects.equals(firstName, student.firstName) && Objects.equals(lastName, student.lastName) && Objects.equals(groupName, student.groupName);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(firstName, lastName, group);
+        return Objects.hash(firstName, lastName, groupName);
     }
 }
