84819d866e28de3df3f0a0b17e7efc7c1825cc4a
Yaroslav82 <yargay230607@gmail.com>
2024-01-22 15:46:13 +0200

    Mock service repositories

--- school-manager-server/src/test/java/com/hillel/multi/service/HomeworkManagerServiceTests.java
+++ school-manager-server/src/test/java/com/hillel/multi/service/HomeworkManagerServiceTests.java
@@ -1,16 +1,20 @@
 package com.hillel.multi.service;
 
 import com.hillel.multi.persistent.entities.Homework;
+import com.hillel.multi.persistent.repositories.HomeworkManagerRepository;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.web.context.WebApplicationContext;
 
 @WebMvcTest(controllers = HomeworkManagerService.class)
+@AutoConfigureMockMvc
 public class HomeworkManagerServiceTests {
 
     @Autowired
@@ -18,6 +22,9 @@ public class HomeworkManagerServiceTests {
 
     @Autowired
     private HomeworkManagerService service;
+
+    @MockBean
+    private HomeworkManagerRepository repository;
 
     @BeforeEach
     public void setUp() {
