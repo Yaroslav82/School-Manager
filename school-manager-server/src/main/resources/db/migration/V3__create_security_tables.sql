b2f5fbdb3fe3bae4440c1f9798c83bf9dd405156
Yaroslav82 <yargay230607@gmail.com>
2024-01-29 21:09:56 +0200

    Add id to insert

--- school-manager-server/src/main/resources/db/migration/V3__create_security_tables.sql
+++ school-manager-server/src/main/resources/db/migration/V3__create_security_tables.sql
@@ -7,6 +7,6 @@ create table user_role_ref (id1 integer not null, id2 integer not null, primary 
 alter table if exists user_role_ref add constraint fk_users foreign key (id1) references users;
 alter table if exists user_role_ref add constraint fk_roles foreign key (id2) references roles;
 
-insert into roles (role_name)
+insert into roles (id, role_name)
 values
-('ROLE_STUDENT'), ('ROLE_TEACHER'), ('ROLE_ADMIN');
+(1, 'ROLE_STUDENT'), (2, 'ROLE_TEACHER'), (3, 'ROLE_ADMIN');
