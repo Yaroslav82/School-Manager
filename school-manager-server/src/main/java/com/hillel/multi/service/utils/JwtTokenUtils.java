199a957bed566129afda8743b7b2297d2212931c
Yaroslav82 <yargay230607@gmail.com>
2024-01-29 19:14:43 +0200

    Create JwtTokenUtils

---
+++ school-manager-server/src/main/java/com/hillel/multi/service/utils/JwtTokenUtils.java
@@ -0,0 +1,63 @@
+package com.hillel.multi.service.utils;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Component
+public class JwtTokenUtils {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.lifetime}")
+    private Duration jwtLifeTime;
+
+    public String generateToken(UserDetails userDetails) {
+        Map<String, Object> claims = new HashMap<>();
+        Set<String> roleSet = userDetails.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.toSet());
+        claims.put("roles", roleSet);
+
+        Date issuedDate = new Date();
+        Date expiredDate = new Date(issuedDate.getTime() + jwtLifeTime.toMillis());
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(issuedDate)
+                .setExpiration(expiredDate)
+                .signWith(SignatureAlgorithm.HS256, secret)
+                .compact();
+    }
+
+    public String getUsername(String token) {
+        return getAllClaims(token).getSubject();
+    }
+
+    public Set<String> getRoles(String token) {
+        if (Objects.isNull(token)) {
+            throw new JwtException("Token cannot be null");
+        }
+        return getAllClaims(token).get("roles", Set.class);
+    }
+
+    private Claims getAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret)))
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+}
