8132b6758229a1eb3cc883f8a018791d92958b76
Yaroslav82 <yargay230607@gmail.com>
2024-01-30 20:40:50 +0200

    Change return type of getRoleByName

--- school-manager-server/src/main/java/com/hillel/multi/service/RoleService.java
+++ school-manager-server/src/main/java/com/hillel/multi/service/RoleService.java
@@ -1,11 +1,10 @@
 package com.hillel.multi.service;
 
+import com.hillel.multi.configuration.exceptions.NotFoundException;
 import com.hillel.multi.persistent.entities.Role;
 import com.hillel.multi.persistent.repositories.RoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 public class RoleService {
@@ -17,7 +16,9 @@ public class RoleService {
         return roleRepository.getByName("ROLE_STUDENT").get();
     }
 
-    public Optional<Role> getRoleByName(String name) {
-        return roleRepository.getByName(name);
+    public Role getRoleByName(String name) {
+        return roleRepository.getByName(name).orElseThrow(
+                () -> new NotFoundException(String.format("Role %s is not found", name))
+        );
     }
 }
