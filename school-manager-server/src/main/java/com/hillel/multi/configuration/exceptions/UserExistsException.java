bb4fd8da21ebfe9f58ec3b8b006a24bacfef294f
Yaroslav82 <yargay230607@gmail.com>
2024-01-30 20:38:42 +0200

    Change HttpStatus to 400

--- school-manager-server/src/main/java/com/hillel/multi/configuration/exceptions/UserExistsException.java
+++ school-manager-server/src/main/java/com/hillel/multi/configuration/exceptions/UserExistsException.java
@@ -3,7 +3,7 @@ package com.hillel.multi.configuration.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class UserExistsException extends RuntimeException {
 
     private final static String DEFAULT_RESPONSE_MESSAGE = "A user with the same username already exists.";
