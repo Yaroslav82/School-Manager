a7add8ec9e6a5b4a47107cd82a8ed29ba8d60eef
Yaroslav82 <yargay230607@gmail.com>
2024-05-30 11:54:49 +0300

    Add exception handler

--- students-service/src/main/java/com/hillel/multi/presentation/exceptions/ErrorDetails.java
+++ students-service/src/main/java/com/hillel/multi/presentation/exceptions/ErrorDetails.java
@@ -1,14 +1,18 @@
-package com.hillel.multi.configuration.exceptions;
+package com.hillel.multi.presentation.exceptions;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.Date;
+import java.util.HashMap;
 
 @Getter
 @Setter
 @AllArgsConstructor
 public class ErrorDetails {
 
-    private String message;
+    private Date timestamp;
     private Integer status;
+    private HashMap<String, String> errors;
 }
---
+++ students-service/src/main/java/com/hillel/multi/presentation/exceptions/ExceptionControllerAdvice.java
@@ -0,0 +1,27 @@
+package com.hillel.multi.presentation.exceptions;
+
+import jakarta.validation.ConstraintViolationException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Date;
+import java.util.HashMap;
+
+@RestControllerAdvice
+@Slf4j
+public class ExceptionControllerAdvice {
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public final ResponseEntity<ErrorDetails> handleValidationException(ConstraintViolationException ex) throws NoSuchFieldException {
+        log.warn("Validation failed");
+        HashMap<String, String> errors = new HashMap<>();
+        ex.getConstraintViolations().forEach(v ->
+                errors.put(v.getPropertyPath().toString(), v.getMessage())
+        );
+        ErrorDetails details = new ErrorDetails(new Date(), HttpStatus.BAD_REQUEST.value(), errors);
+        return new ResponseEntity<>(details, HttpStatus.BAD_REQUEST);
+    }
+}
