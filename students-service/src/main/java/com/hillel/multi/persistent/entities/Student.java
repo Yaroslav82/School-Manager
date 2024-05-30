0846ea4ec9f810614950dd19996043c17b21709d
Yaroslav82 <yargay230607@gmail.com>
2024-05-30 11:53:45 +0300

    Add persistent level

---
+++ students-service/src/main/java/com/hillel/multi/persistent/entities/Student.java
@@ -0,0 +1,40 @@
+package com.hillel.multi.persistent.entities;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Objects;
+
+@Data
+@Document(collection = "students")
+public class Student {
+
+    @Id
+    private String id;
+
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    @NotBlank
+    @Pattern(regexp = "^[A-Z]{2,3}-\\d+$", message = "Should be in format: \'GB-123\'")
+    private String groupName;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(firstName, student.firstName) && Objects.equals(lastName, student.lastName) && Objects.equals(groupName, student.groupName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, groupName);
+    }
+}
