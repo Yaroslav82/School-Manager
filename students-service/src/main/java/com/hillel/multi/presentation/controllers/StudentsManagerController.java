1cf948c4a66b070aac7c6eb8c8a02fc023787e00
Yaroslav82 <yargay230607@gmail.com>
2024-05-30 12:14:48 +0300

    Add logs

--- students-service/src/main/java/com/hillel/multi/presentation/controllers/StudentsManagerController.java
+++ students-service/src/main/java/com/hillel/multi/presentation/controllers/StudentsManagerController.java
@@ -3,6 +3,7 @@ package com.hillel.multi.presentation.controllers;
 import com.hillel.api.StudentsManagerApi;
 import com.hillel.model.StudentDTO;
 import com.hillel.multi.service.StudentsManagerService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,7 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
+@Slf4j
 public class StudentsManagerController implements StudentsManagerApi {
 
     @Autowired
@@ -19,30 +21,35 @@ public class StudentsManagerController implements StudentsManagerApi {
 
     @Override
     public ResponseEntity<List<StudentDTO>> getStudents() {
+        log.info("Received request to get all students");
         List<StudentDTO> studentDTOS = studentsManagerService.getStudents();
         return new ResponseEntity<>(studentDTOS, HttpStatus.OK);
     }
 
     @Override
     public ResponseEntity<StudentDTO> getStudentById(String id) {
+        log.info("Received request to get student with id '{}'", id);
         StudentDTO studentDTO = studentsManagerService.getStudentById(id);
         return ResponseEntity.ok(studentDTO);
     }
 
     @Override
     public ResponseEntity<StudentDTO> createStudent(StudentDTO studentDTO, URI xCallbackUrl) {
+        log.info("Received request to add student");
         StudentDTO body = studentsManagerService.addStudent(studentDTO);
         return new ResponseEntity<>(body, HttpStatus.CREATED);
     }
 
     @Override
     public ResponseEntity<StudentDTO> updateStudent(String id, StudentDTO studentDTO) {
+        log.info("Received request to update student with id '{}'", id);
         StudentDTO body = studentsManagerService.updateStudent(id, studentDTO);
         return new ResponseEntity<>(body, HttpStatus.OK);
     }
 
     @Override
     public ResponseEntity<Void> deleteStudent(String id) {
+        log.info("Received request to delete student with id '{}'", id);
         studentsManagerService.deleteStudent(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
--- students-service/src/main/java/com/hillel/multi/service/StudentsManagerService.java
+++ students-service/src/main/java/com/hillel/multi/service/StudentsManagerService.java
@@ -6,6 +6,7 @@ import com.hillel.multi.persistent.repositories.StudentManagerRepository;
 import com.hillel.multi.service.mappers.StudentMapper;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
@@ -16,33 +17,40 @@ import java.util.List;
 @Service
 @Validated
 @AllArgsConstructor
+@Slf4j
 public class StudentsManagerService {
 
     private StudentManagerRepository studentManagerRepository;
 
     public List<StudentDTO> getStudents() {
+        log.info("Getting students");
         List<Student> students = studentManagerRepository.findAll();
         return entityToDTO(students);
     }
 
     public StudentDTO getStudentById(String id) {
+        log.info("Getting student with id {}", id);
         return entityToDTO(getEntityById(id));
     }
 
     public StudentDTO addStudent(StudentDTO studentDTO) {
         Student entity = studentManagerRepository.save(dtoToEntity(studentDTO));
+        log.info("Student with id {} added", entity.getId());
         return entityToDTO(entity);
     }
 
     public StudentDTO updateStudent(String id, StudentDTO studentDTO) {
         Student entity = getEntityById(id);
         StudentMapper.INSTANCE.updateIntoStudent(entity, studentDTO);
-        return entityToDTO(studentManagerRepository.save(entity));
+        entity = studentManagerRepository.save(entity);
+        log.info("Student with id '{}' updated", id);
+        return entityToDTO(entity);
     }
 
     public void deleteStudent(String id) {
         Student entity = getEntityById(id);
         studentManagerRepository.delete(entity);
+        log.info("Student with id '{}' was deleted", id);
     }
 
     public StudentDTO entityToDTO(@Valid Student student) {
