91a1d7b78268eba75e7b928ed456bab4b69935b2
Yaroslav82 <yargay230607@gmail.com>
2024-06-24 16:33:59 +0300

    Delete school-manager module

--- school-manager-server/src/main/resources/db/migration/V4__add_admin.sql
+++
@@ -1,7 +0,0 @@
-insert into users (id, username, password)
-values
-(1, 'admin', '$2a$10$119vmaGqT0zeAY0gBIENnuNJcu7QnIwq91MP7imO5JiTIUya92VX2'); -- password: admin
-
-insert into user_role_ref (id1, id2)
-values
-(1, 3);
