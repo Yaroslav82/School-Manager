783e7b184763147d0c12a6dfd70685146e9f853f
Yaroslav82 <yargay230607@gmail.com>
2024-06-24 16:40:53 +0300

    Create Application.java

--- timetable-service/src/main/java/com/hillel/multi/Application.java
+++ timetable-service/src/main/java/com/hillel/multi/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(Application.class)
-                .bannerMode(Banner.Mode.OFF) // <- запуск без Spring банеру
+                .bannerMode(Banner.Mode.OFF)
                 .run(args);
     }
 }
