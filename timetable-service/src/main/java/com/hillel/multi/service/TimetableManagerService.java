8128d99e234a14f248aa448ad8519b8c87f284ff
Yaroslav82 <yargay230607@gmail.com>
2024-06-24 16:38:23 +0300

    Create TimetableManagerService and LessonMapper

--- timetable-service/src/main/java/com/hillel/multi/service/TimetableManagerService.java
+++ timetable-service/src/main/java/com/hillel/multi/service/TimetableManagerService.java
@@ -1,39 +1,50 @@
 package com.hillel.multi.service;
 
 import com.hillel.model.LessonDTO;
-import com.hillel.multi.configuration.exceptions.NotFoundException;
 import com.hillel.multi.persistent.entities.Lesson;
 import com.hillel.multi.persistent.repositories.TimetableManagerRepository;
 import com.hillel.multi.service.mappers.LessonMapper;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
-import java.util.Objects;
 
 @Service
 @Validated
+@Slf4j
 public class TimetableManagerService {
 
     @Autowired
     private TimetableManagerRepository timetableManagerRepository;
 
     public List<LessonDTO> getLessons(String group, String subject) {
+        log.info("Getting lessons for group '{}' and subject '{}'", group, subject);
         List<Lesson> lessons = timetableManagerRepository.getLessonsByGroupAndSubject(group, subject);
         return entityToDTO(lessons);
     }
 
     public LessonDTO addLesson(LessonDTO lessonDTO) {
         Lesson entity = timetableManagerRepository.save(dtoToEntity(lessonDTO));
+        log.info("Lesson with id '{}' added", entity.getId());
         return entityToDTO(entity);
     }
 
     public LessonDTO updateLesson(Integer id, LessonDTO lessonDTO) {
         Lesson entity = getEntityById(id);
         LessonMapper.INSTANCE.updateIntoLesson(entity, lessonDTO);
-        return entityToDTO(timetableManagerRepository.save(entity));
+        entity = timetableManagerRepository.save(entity);
+        log.info("Lesson with id '{}' updated", id);
+        return entityToDTO(entity);
+    }
+
+    public void deleteLesson(Integer id) {
+        timetableManagerRepository.deleteById(Long.valueOf(id));
+        log.info("Lesson with id '{}' was deleted", id);
     }
 
     public LessonDTO entityToDTO(@Valid Lesson lesson) {
@@ -49,11 +60,8 @@ public class TimetableManagerService {
     }
 
     private Lesson getEntityById(Integer id) {
-        Lesson entity = timetableManagerRepository.getLessonById(id.longValue());
-        if (Objects.nonNull(entity)) {
-            return entity;
-        } else {
-            throw new NotFoundException("Lesson with id " + id + " is not found.");
-        }
+        return timetableManagerRepository.getLessonById(id.longValue()).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Lesson with id " + id + " is not found.")
+        );
     }
 }
