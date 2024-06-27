d2ac28e38cf8d4ffa372475e9138f220eee436f1
Yaroslav82 <yargay230607@gmail.com>
2024-06-27 20:40:11 +0300

    Solve merge conflicts between timetable and student services

--- timetable-service/src/main/java/com/hillel/multi/presentation/exceptions/ErrorDetails.java
+++ timetable-service/src/main/java/com/hillel/multi/presentation/exceptions/ErrorDetails.java
@@ -15,8 +15,4 @@ public class ErrorDetails {
     private Date timestamp;
     private Integer status;
     private HashMap<String, String> errors;
-<<<<<<<< HEAD:students-service/src/main/java/com/hillel/multi/presentation/exceptions/ErrorDetails.java
 }
-========
-}
->>>>>>>> origin/feature/microservice-for-lessons:timetable-service/src/main/java/com/hillel/multi/presentation/exceptions/ErrorDetails.java
